c73b18b22a696122c268f89d5b551773ece1a6c9
REbORN <stephenokehukwu2022@gmail.com>
2023-06-19 09:02:17 +0100

    added new files

---
+++ portfolio.js
@@ -0,0 +1,10 @@
+let menu = document.querySelector("#icon");
+let navbar = document.querySelector(".navbar");
+
+menu.addEventListener("click", function () {
+    navbar.classList.toggle("active")
+});
+
+window.onscroll = () => {
+    navbar.classList.remove("active")
+}
